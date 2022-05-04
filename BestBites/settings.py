6d8a076ca28d179b8bc1a712699b34a086a214c8
jagrati <jagratirathod02@gmail.com>
2022-05-04 16:56:32 +0530

    dfd

--- BestBites/settings.py
+++ BestBites/settings.py
@@ -160,6 +160,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -182,4 +183,3 @@ CSRF_TRUSTED_ORIGINS = [
     'https://bestbites2.herokuapp.com'
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
